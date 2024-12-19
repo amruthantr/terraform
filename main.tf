8cc05638a611d3b14e953b849243b94d65a2732c
amruthantr <85926078+amruthantr@users.noreply.github.com>
2024-12-19 12:54:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-ami          = "ami-0208b77a23d891325"
+ami          = "ami-01816d07b1128cd2d"
 instance_type = "t2.micro"
 tags = {
 Name ="raham-server"
